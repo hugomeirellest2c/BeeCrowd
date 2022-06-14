60f298623dbd4cea2b7244f5a163791143b9d4f0
Hugo Meirelles <82676377+hugomeirellest2c@users.noreply.github.com>
2022-06-14 19:48:23 -0300

    1006 solved

---
+++ done/1006.js
@@ -0,0 +1,20 @@
+//#region CONFIG
+
+let input = '';
+try {
+    input = require('fs').readFileSync('/dev/stdin', 'utf8');
+} catch (e) {
+    input = require('fs').readFileSync('./dev/stdin', 'utf8');
+}
+
+var lines = input.split('\n');
+
+//#endregion
+
+var A = parseFloat(lines.shift());
+var B = parseFloat(lines.shift());
+var C = parseFloat(lines.shift());
+
+var X = ((A*2+B*3+C*5)/10).toFixed(1);
+
+console.log(`MEDIA = ${X}`);
