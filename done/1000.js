2bdd73f2b73b03405daeeeabbb59a377f1ec8993
Hugo Meirelles <82676377+hugomeirellest2c@users.noreply.github.com>
2022-06-14 13:28:10 -0300

    Create 1000.js

---
+++ done/1000.js
@@ -0,0 +1,10 @@
+const { parse } = require('path');
+
+let input = '';
+try {
+    input = require('fs').readFileSync('/dev/stdin', 'utf8');
+} catch (e) {
+    input = require('fs').readFileSync('./dev/stdin', 'utf8');
+}
+
+console.log('Hello World!');
