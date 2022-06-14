3e334843ac6350a52c8e2402016c3e410bbd21f0
hugomeirellest2c <hugo.meirelles2002@gmail.com>
2022-06-14 13:07:47 -0300

    initial structure created

---
+++ index.js
@@ -0,0 +1,8 @@
+let input = '';
+try {
+    input = require('fs').readFileSync('/dev/stdin', 'utf8');
+} catch (e) {
+    input = require('fs').readFileSync('./dev/stdin', 'utf8');
+}
+
+var lines = input.split('\n');
